945ff30677e564d47514242ec7ff2861e51a15f6
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-04-26 10:37:34 +0300

    шаг 2

--- post_comics.py
+++ post_comics.py
@@ -1,29 +1,57 @@
 import os
 import requests
+import re
 
 
-def download_picture(pic_path, pic_name, pic_dir):
+def download_picture(pic_path, pic_name = None):
     '''
     Function download pictures from url to local directory.
     Create directory if it no exist.
+    If
 
     Keywords arguments:
     pic_path -- url of picture fo download
     pic_name -- name of picture
-    pic_dir -- local directory for picture dowload
-
+    #pic_dir -- local directory for picture dowload
     '''
-    os.makedirs(pic_dir, exist_ok=True)
+    #os.makedirs(pic_dir, exist_ok=True)
+    if pic_name is None:
+        pic_name = re.findall(r'\w+\.png', pic_path)[0]
 
-    filename = os.path.join(pic_dir, pic_name)
+    filename = os.path.join('.', pic_name)
     response = requests.get(pic_path)
 
     with open(filename, 'wb') as file:
         file.write(response.content)
 
+
+def get_xkcd_comics_info(issue_id = None):
+    '''
+    Function get url of issue of xkcd comics.
+    If issue_id is not None than function return url of issue_id comics.
+    If issue_id is None than function return url of last comics.
+
+    Keywords arguments:
+    issue_id -- id of issue xkcd comics
+    '''
+
+    if issue_id is not None:
+        api_path = 'https://xkcd.com/{}/info.0.json'.format(issue_id)
+    else:
+        api_path = 'https://xkcd.com/info.0.json'
+
+    response = requests.get(api_path).json()
+
+    return response
+
+
 def main():
 
-    
+    comics_info = get_xkcd_comics_info()
+    comics_url = comics_info['img']
+    comics_alt = comics_info['alt']
+    print(comics_alt)
+    download_picture(comics_url)
 
 
 if __name__ == '__main__':
