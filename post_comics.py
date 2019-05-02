0228426846cebae821ed1d4bd1f327ce68c53d30
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-05-02 16:01:21 +0300

    коммит перед объединением функций

--- post_comics.py
+++ post_comics.py
@@ -6,26 +6,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def download_picture(pic_path, pic_name = None):
+def download_picture(img_url, img_dir = '.', img_name = None):
     '''
     Function download pictures from url to local directory.
     Create directory if it no exist.
-    If
 
     Keywords arguments:
-    pic_path -- url of picture fo download
-    pic_name -- name of picture
-    #pic_dir -- local directory for picture dowload
+    img_url -- url of picture fo download
+    img_name -- name of picture
+    img_dir -- local directory for picture dowload
+
+    Return img_local_full_path
     '''
-    #os.makedirs(pic_dir, exist_ok=True)
-    if pic_name is None:
-        pic_name = re.findall(r'\w+\.png', pic_path)[0]
+    #os.makedirs(img_dir, exist_ok=True)
+    img_name = os.path.basename(img_url)
 
-    filename = os.path.join('.', pic_name)
-    response = requests.get(pic_path)
+    img_local_full_path = os.path.join(img_dir, img_name)
+    response = requests.get(img_url)
 
-    with open(filename, 'wb') as file:
+    with open(img_local_full_path, 'wb') as file:
         file.write(response.content)
+    return img_local_full_path
 
 
 def get_xkcd_comics_info(issue_id = None):
@@ -47,30 +48,63 @@ def get_xkcd_comics_info(issue_id = None):
     return response
 
 
-def get_list_of_group():
-    method_name = 'groups.get'
+def get_address_upload_photos():
+    '''
+    Get address to upload photos
+    '''
+    method_name = 'photos.getWallUploadServer'
     token = os.getenv("TOKEN")
-    client_id = os.getenv("client_id")
     vk_version = '5.95'
-    parameters = f'user_id={client_id}'
+    group_id='181623583'
+    parameters = f'group_id={group_id}'
+
+    url_request = f'https://api.vk.com/method/{method_name}?{parameters}&access_token={token}&v={vk_version}'
+    response = requests.get(url_request)
+    return response.json()['response']['upload_url']
+
+
+def upload_photo_to_server(url, img_path):
+
+    img_name = os.path.basename(img_path)
+
+    image_file_descriptor = open(img_name, 'rb')
+    files = {'file': image_file_descriptor}
+    response = requests.post(url, files=files)
+    image_file_descriptor.close()
+
+    return response.json()
+
+
+def save_wall_photo(photo_on_server, caption):
+    '''
+    __
+    '''
+    method_name = 'photos.saveWallPhoto'
+    token = os.getenv("TOKEN")
+    vk_version = '5.95'
+    group_id='181623583'
+    hash = photo_on_server['hash']
+    server = photo_on_server['server']
+    photo = photo_on_server['photo']
+    parameters = f'group_id={group_id}&photo={photo}&hash={hash}&server={server}&caption={caption}'
 
     url_request = f'https://api.vk.com/method/{method_name}?{parameters}&access_token={token}&v={vk_version}'
     print(url_request)
     response = requests.get(url_request)
-    print(response.json())
+    return response.json()
 
 
 def main():
-
+    print('*********')
     comics_info = get_xkcd_comics_info()
     comics_url = comics_info['img']
     comics_alt = comics_info['alt']
-    client_id = os.getenv("client_id")
-    print(client_id)
-    get_list_of_group()
+    user_id = os.getenv("user_id")
 
-    #download_picture(comics_url)
-
+    download_picture(comics_url)
+    photos_upload_url = get_address_upload_photos()
+    photo_on_server = upload_photo_to_server(photos_upload_url, comics_url)
+    print(save_wall_photo(photo_on_server, comics_alt))
 
 if __name__ == '__main__':
     main()
